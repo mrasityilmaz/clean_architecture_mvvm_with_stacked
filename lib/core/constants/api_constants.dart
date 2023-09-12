ab9b2ebfb08f3b706cda7d8416c81532d71fb9e4
Muhammed Raşit Yılmaz <rasityilmaz@yaani.com>
2023-09-12 17:58:11 +0300

    removed_dummy_code

--- lib/core/constants/api_constants.dart
+++ lib/core/constants/api_constants.dart
@@ -1,5 +1,3 @@
-import 'package:stacked/stacked_annotations.dart';
-
 final class APIConstants {
   factory APIConstants() {
     _instance ??= const APIConstants._();
@@ -8,7 +6,6 @@ final class APIConstants {
   const APIConstants._();
   static APIConstants? _instance;
 
-  @factoryParam
   static const String _baseUrl = 'https://api.openweathermap.org/data/2.5/';
 
   static String get baseURL => _baseUrl;
